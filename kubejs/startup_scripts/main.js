b71b0363502647fb9ed2e80ecfb9c8f4e8703dc2
Sam Haygood <57695522+samhaygood05@users.noreply.github.com>
2026-04-25 18:15:48 -0400

    feat: replaced vista viewfinder and hollow cassette recipes

--- kubejs/startup_scripts/main.js
+++ kubejs/startup_scripts/main.js
@@ -43,9 +43,23 @@ StartupEvents.registry('item', event => {
         }
     }
 
+    event.create('iron_dust').displayName('Iron Dust').tag('c:dusts/iron').tag('c:dusts')
+    event.create('tinted_glass_sheet').displayName('Tinted Glass Sheet')
+    event.create('rose_lens').displayName('Rose Lens')
+    event.create('wet_magnetic_tape').displayName('Wet Magnetic Tape').texture('vista:item/picture_tape').color('#699EE4')
+    event.create('magnetic_tape').displayName('Magnetic Tape').texture('vista:item/picture_tape')
+    event.create('cassette_shell').displayName('Cassette Shell').texture('vista:item/cassette')
+    event.create('optical_mechanism').displayName('Optical Mechanism')
+
+    event.create('viewfinder_housing').displayName('Viewfinder Housing')
+
+
+    event.create('incomplete_rose_lens', 'create:sequenced_assembly').displayName('Incomplete Rose Lens').texture('kubejs:item/rose_lens')
+    event.create('unfinished_magnetic_tape', 'create:sequenced_assembly').displayName('Unfinished Magnetic Tape').texture('vista:item/picture_tape')
+    event.create('incomplete_optical_mechanism', 'create:sequenced_assembly').displayName('Incomplete Optical Mechanism').texture('kubejs:item/optical_mechanism')
     event.create('unfinished_television_housing', 'create:sequenced_assembly').displayName('Unfinished Television Housing').parentModel('kubejs:block/television_housing')
     event.create('incomplete_television', 'create:sequenced_assembly').displayName('Incomplete Television').parentModel('kubejs:block/open_television')
-    event.create('tinted_glass_sheet').displayName('Tinted Glass Sheet')
+    event.create('incomplete_viewfinder', 'create:sequenced_assembly').displayName('Incomplete Viewfinder').texture('kubejs:item/viewfinder_housing')
 
 })
 
@@ -64,4 +78,10 @@ StartupEvents.registry('block', event => {
     .mapColor('brown')
     .hardness(1.5)
     .tagBlock('minecraft:mineable/axe')
+})
+
+StartupEvents.registry('fluid', event => {
+    event.create('magnetic_slurry')
+    .displayName('Magnetic Slurry')
+    .noBlock()
 })
